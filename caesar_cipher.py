5d464b878e334b4b679ef1bff402988ee389e623
princezukoburner <king.mordi.com@gmail.com>
2022-12-21 15:50:16 -0800

    commit test using cli

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,14 +1,24 @@
 import sys
 
-maximum = 26
 
 
 def inputMsg():
     msg = input("Please input your message: ")
     return msg
 
+def decipher():
+    pass
+
 def pullKey():
     start = 0 
 
     while True:
-    
+        print("Please enter shift num: ", end = " ")
+        
+        user_input = int(input())
+        if user_input >= 1 and user_input <= 26:
+            return user_input
+        else:
+            print("Value has to fall in range 1 and 26")
+            sys.exit(0)  #zero error code for shell
+            
