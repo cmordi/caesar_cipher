ed73bcaaf2294610ac97c1447529487c3a102cf9
princezukoburner <king.mordi.com@gmail.com>
2022-12-22 03:27:35 -0800

    added a few files for the user in regards to decoding

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,13 +1,36 @@
 import sys
 
-
-
 def inputMsg():
     msg = input("Please input your message: ")
     return msg
 
-def decipher():
-    pass
+def decipher(message, key, mode):
+    decoded = ""
+    
+    if mode == 1:
+        key = -key
+
+    for symbol in message:
+        if symbol.isalpha():
+            number = ord(symbol)
+            number += key
+            
+            if symbol.islower():
+                if number > ord('z'):
+                    number -= 26
+                elif number < ord('a'):
+                    number += 26
+            elif symbol.isupper():
+                if number > ord('Z'):
+                    number -= 26
+                elif number < ord('A'):
+                    number += 26
+            decoded += chr(number)
+            
+        else:
+            decoded += symbol
+    print(f"\n{decoded}")
+            
 
 def pullKey():
     start = 0 
@@ -22,3 +45,12 @@ def pullKey():
             print("Value has to fall in range 1 and 26")
             sys.exit(0)  #zero error code for shell
             
+
+usr_input = "Y" 
+
+while usr_input:
+    print("""Select what you want:
+          
+        1) 
+          
+            """)
